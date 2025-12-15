4f3b2abad13e984f0c82bd2f08bf1981d40e3591
Md, Wazih Abdullah <abdullahwazih@gmail.com>
2025-12-15 23:04:03 +0600

    Time: 7 ms (67.51%) | Memory: 19.2 MB (34.02%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,75 +1,12 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left = 0
+        right = len(arr) - 1
 
-        dif = float('inf')
-        piv = 0
-
-        for i in range(0, len(arr)):
-            
-            if abs(arr[i] - x) < dif:
-                piv = i
-                dif = abs(arr[i] - x)
-
-        l = piv - 1
-        r = piv +  1
-
-        ans = []
-        ans.append(arr[piv])
-        k -= 1
-
-        while k:
-            
-            if r < len(arr) and l > -1:
-                print("Enters", l, r, abs(arr[l] - x), abs(arr[r] - x))
-
-                if abs(arr[l] - x) > abs(arr[r] - x):
-
-                    ans.append(arr[r])
-                    print("First Cond", r, arr[r])
-
-                    r += 1
-
-                elif abs(arr[l] - x) < abs(arr[r] - x):
-
-                    ans.append(arr[l])
-                    print("Second Cond", l, arr[l])
-                    l -= 1
-                else:
-                    if l > -1:
-                        ans.append(arr[l])
-                        print("Third Cond", l, arr[l])
-
-                        l -= 1
-
-                    elif r < len(arr):
-                        ans.append(arr[r])
-                        print("Fourth Cond", r, arr[r])
-
-                        r += 1
-
+        while right - left + 1 > k:
+            if abs(arr[left] - x) > abs(arr[right] - x):
+                left += 1
             else:
-                if l > -1:
-                    ans.append(arr[l])
-                    print("Third Cond", l, arr[l])
+                right -= 1
 
-                    l -= 1
-
-                elif r < len(arr):
-                    ans.append(arr[r])
-                    print("Fourth Cond", r, arr[r])
-
-                    r += 1
-            
-            k -= 1
-
-            if l < 0 and r >= len(arr):
-                break
-
-        ans.sort()
-
-        return ans
-
-        
-
-
-        
+        return arr[left:right + 1]
