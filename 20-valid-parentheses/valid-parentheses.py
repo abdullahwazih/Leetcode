025dc4a66e42e11450be8e36c7007e3278b2b5ac
Md, Wazih Abdullah <abdullahwazih@gmail.com>
2026-03-02 14:25:06 +0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.02%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+        
+
+        for ch in s:
+
+            if ch == '(' or ch == '{' or ch == '[':
+                stack.append(ch)
+
+            elif ch == ')':
+                if len(stack) >= 1 and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    return False
+                
+            elif ch == '}':
+                if len(stack) >= 1 and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+            elif ch == ']':
+                if len(stack) >= 1 and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        return False if len(stack) >=  1 else True
+
+            
+                
+        
